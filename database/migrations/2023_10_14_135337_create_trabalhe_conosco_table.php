dbb0b25f4d59385cedd9203247edd88972d37c52
Brunorobson <bruno.assis@alu.unibalsas.edu.br>
2023-10-14 11:46:33 -0300

    criação migrate

--- database/migrations/2023_10_14_135337_create_trabalhe_conosco_table.php
+++ database/migrations/2023_10_14_135337_create_trabalhe_conosco_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('trabalhe_conosco', function (Blueprint $table) {
             $table->id();
+            $table->string('nome_completo');
+            $table->string('email');
+            $table->string('telefone');
+            $table->char('carreira', 2)->default('AA'); //QUAIS CARREIRAS VOCÊ ESTÁ BUSCANDO?
+            $table->text('habilidades');
+            $table->char('experiência')->default('A'); //QUAL A SUA EXPERIÊNCIA GERAL NA ÁREA TECH?
+            $table->char('momento_profissional', 2)->default('AA'); //COMO ESTA SEU MOMENTO PROFISSIONAL?
+            $table->string('cidade_residencia');
+            $table->textarea('sobre');
             $table->timestamps();
         });
     }
