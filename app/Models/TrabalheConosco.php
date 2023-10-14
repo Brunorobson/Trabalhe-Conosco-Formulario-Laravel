170f1180d044da439c334616517fcb399d4ccee3
Brunorobson <bruno.assis@alu.unibalsas.edu.br>
2023-10-14 11:59:28 -0300

    model

--- app/Models/TrabalheConosco.php
+++ app/Models/TrabalheConosco.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class TrabalheConosco extends Model
 {
     use HasFactory;
+    protected $table = 'trabalhe_conosco';
+    protected $fillable = [
+        'nome_completo',
+        'email',
+        'telefone',
+        'carreiras',
+        'habilidades',
+        'momento_profissional',
+        'cidade_residencia',
+        'sobre',
+    ];
 }
